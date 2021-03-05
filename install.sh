780b1aeed346a0ad8e0be76885d55161f55704d6
Beth Harmon <bethharmon@disroot.org>
2021-03-04 16:27:59 -0800

    install.sh: update to include new scripts

--- install.sh
+++ install.sh
@@ -2,15 +2,18 @@
 # Installer for ebu-norm, tp-norm and ebu-scan
 # Copyright © 2021 Elizabeth Harmon
 
-echo "This script will install batch normalizer/scanner scripts to usr/local/bin."
+echo "This script will install batch normalizer/scanner scripts to usr/local/bin"
+echo "and bcrc (bc calculator extensions) to ~/.bcrc"
 read -p "Do you wish to continue?" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	echo "Making scripts executable..."
 	sleep 1
-	chmod +x ebu-norm tp-norm ebu-scan
-	echo "Copying files to usr/local/bin..."
+	chmod +x ebu-norm tp-norm ebu-scan loudmax-norm lsp-norm 
+	echo "Copying scripts to usr/local/bin..."
+	sleep 1
+	echo "Copying bc extensions to ~/.bcrc..."
 	sleep 1
 	cp ebu-norm tp-norm ebu-scan loudmax-norm lsp-norm /usr/local/bin
 	cp bcrc ~/.bcrc
